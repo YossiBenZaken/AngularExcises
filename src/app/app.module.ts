a672f09795cad642353d4d2f497192bf7296270b
Yossi Ben Zaken <46167117+YossiBenZaken@users.noreply.github.com>
2019-07-14 20:32:54 +0300

    More stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { LikeComponent } from './like/like.component';
+import {MatIconModule} from '@angular/material/icon'; 
+import {MatTabsModule} from '@angular/material/tabs';
+import { AuthorsComponent } from './authors/authors.component';
+import { FavoriteComponent } from './favorite/favorite.component';
+import {MatButtonModule} from '@angular/material/button'; 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LikeComponent,
+    AuthorsComponent,
+    FavoriteComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatTabsModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
